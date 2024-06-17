0ae50a3a8f737f6813536e21cb84f5fe370e6f13
dev-luqman <o.balogun@ymail.com>
2024-06-17 17:23:32 +0100

    Update workflow

--- farmers/server.js
+++ farmers/server.js
@@ -10,20 +10,24 @@ app.use(function* (next) {
   console.log("%s %s - %s", this.method, this.url, ms);
 });
 
-router.get('/api/farmers', function *(next) {
-    this.body = db;
-  });
-  
-  router.get('/api/farmers/:farmersId', function *(next) {
-    const id = parseInt(this.params.farmersId);
-    this.body = db.find((farmer) => farmer.id == id);
-  });
+router.get('/api/farmers', function* (next) {
+  yield next; // Add yield statement here
+  this.body = db;
+});
+
+router.get('/api/farmers/:farmersId', function* (next) {
+  yield next; // Add yield statement here
+  const id = parseInt(this.params.farmersId);
+  this.body = db.find((farmer) => farmer.id == id);
+});
 
 router.get("/api/", function* () {
+  yield; // Add yield statement here
   this.body = "Farmers API ready to receive requests...";
 });
 
 router.get("/", function* () {
+  yield; // Add yield statement here
   this.body = "Setup and ready to receive request...";
 });
 
@@ -31,5 +35,4 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.listen(3000);
-
 console.log("Worker started");
