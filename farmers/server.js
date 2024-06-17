ec54bd2c265a448390fad5fea03d723d7c66bfad
dev-luqman <o.balogun@ymail.com>
2024-06-17 17:48:22 +0100

    Update workflow

--- farmers/server.js
+++ farmers/server.js
@@ -1,38 +1,39 @@
-const app = require("koa")();
-const router = require("koa-router")();
-const db = require("./db.json");
+const Koa = require('koa');
+const Router = require('koa-router');
+const db = require('./db.json');
+
+const app = new Koa();
+const router = new Router();
 
 // Log requests
-app.use(function* (next) {
+app.use(async (ctx, next) => {
   const start = new Date();
-  yield next;
+  await next();
   const ms = new Date() - start;
-  console.log("%s %s - %s", this.method, this.url, ms);
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
-router.get('/api/farmers', function* (next) {
-  yield next; // Add yield statement here
-  this.body = db;
+router.get('/api/farmers', async (ctx, next) => {
+  await next();
+  ctx.body = db;
 });
 
-router.get('/api/farmers/:farmersId', function* (next) {
-  yield next; // Add yield statement here
-  const id = parseInt(this.params.farmersId);
-  this.body = db.find((farmer) => farmer.id == id);
+router.get('/api/farmers/:farmersId', async (ctx, next) => {
+  await next();
+  const id = parseInt(ctx.params.farmersId);
+  ctx.body = db.find((farmer) => farmer.id === id);
 });
 
-router.get("/api/", function* () {
-  yield; // Add yield statement here
-  this.body = "Farmers API ready to receive requests...";
+router.get('/api/', async (ctx) => {
+  ctx.body = 'Farmers API ready to receive requests...';
 });
 
-router.get("/", function* () {
-  yield; // Add yield statement here
-  this.body = "Setup and ready to receive request...";
+router.get('/', async (ctx) => {
+  ctx.body = 'Setup and ready to receive request...';
 });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.listen(3000);
-console.log("Worker started");
+console.log('Worker started');
